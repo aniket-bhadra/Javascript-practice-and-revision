55e286b754d0af8854744028a022123f4c322d00
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-07-28 22:50:48 +0530

    practice few tricky questions about asynchronous js execution flow

--- asynchronousJavaScript.js
+++ asynchronousJavaScript.js
@@ -1,16 +1,48 @@
-function asynchrounusMine(){
-    console.log("before setTimeout")
-    setTimeout(()=>{
-        console.log("this is setTimeout")
-    },1000)
-    console.log("after setTimeout")
-}
-let flag = prompt("enter the char")
-function hello(){
-    console.log("hello from hello function")
-}
+// function asynchrounusMine(){
+//     console.log("before setTimeout")
+//     setTimeout(()=>{
+//         console.log("this is setTimeout")
+//     },1000)
+//     console.log("after setTimeout")
+// }
+// let flag = prompt("enter the char")
+// function hello(){
+//     console.log("hello from hello function")
+// }
 
-if(flag ==='a'){
-    asynchrounusMine();
-    hello();
-}
+// if(flag ==='a'){
+//     asynchrounusMine();
+//     hello();
+// }
+
+
+//output base questions--
+// console.log("A");
+// setTimeout(function() {
+//   console.log("B");
+// }, 0);
+// Promise.resolve().then(function() {
+//     console.log("C");
+//   });
+// console.log("D");
+
+// console.log("A");
+// setTimeout(function() {
+//   console.log("B");
+//   Promise.resolve().then(function() {
+//     console.log("C");
+//   });
+// }, 0);
+// console.log("D");
+
+console.log("A");
+setTimeout(function() {
+  console.log("B");
+}, 0);
+Promise.resolve().then(function() {
+  console.log("C");
+  setTimeout(function() {
+    console.log("D");
+  }, 0);
+});
+console.log("E");
