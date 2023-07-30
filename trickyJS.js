b219fb066ae877af3a1d7cae78a324d821ec25e8
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-07-30 13:02:47 +0530

    few tricky code snippets tested to understand let's block scooping nature in detail

---
+++ trickyJS.js
@@ -0,0 +1,20 @@
+// if(true){
+//     let a= 0
+//     for(let i=0; i<5; i++){
+//     console.log(a);
+//     a++
+//     }
+  
+//   }
+//   for (let i = 0; i < 5; i++) {
+//     function one (){
+//       console.log(i)
+//     }
+//     one()
+//   }
+
+//   for (let i = 0; i < 5; i++) {
+//     setTimeout(function () {
+//       console.log(i);
+//     }, 1000);
+//   }
