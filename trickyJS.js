15c0467fb2264e1846c1355f427bad10213fb9b7
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-07-30 14:31:50 +0530

    revisied--static method creation in class bass syntax and inside constructor function

--- trickyJS.js
+++ trickyJS.js
@@ -19,14 +19,45 @@
 //     }, 1000);
 //   }
 
-const obj = {
-    value: 42,
-    getValue: function() {
-      return this.value;
-    }
-  };
+// const obj = {
+//     value: 42,
+//     getValue: function() {
+//       return this.value;
+//     }
+//   };
   
-  const getValue = obj.getValue;
-  const anotherObj = { value: 100, getValue };
+//   const getValue = obj.getValue;
+//   const anotherObj = { value: 100, getValue };
   
-  console.log(anotherObj.getValue());
+//   console.log(anotherObj.getValue());
+
+//static methods in constructor function & Class base syntax
+
+// class MathUtils {
+//     static add(a, b) {
+//       return a + b;
+//     }
+  
+//     static subtract(a, b) {
+//       return a - b;
+//     }
+//   }
+  
+//   console.log(MathUtils.add(5, 3)); 
+//   console.log(MathUtils.subtract(10, 4)); 
+
+
+function MathUtils() {}
+
+MathUtils.add = function(a, b) {
+  return a + b;
+};
+
+MathUtils.subtract = function(a, b) {
+  return a - b;
+};
+
+console.log(MathUtils.add(5, 3)); 
+console.log(MathUtils.subtract(10, 4));
+
+  
