f41e3ef30e6b702663d2c4cdca0c91028a8836e8
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-08-05 22:59:40 +0530

    a tricky case explored while sending arguements to callback with the help of bind method

--- thisBinding.js
+++ thisBinding.js
@@ -24,13 +24,25 @@ let greetPerson1 = greet.bind(person1);
 greetPerson1(); 
 
 
+//function my(status, callback) {
+//  if(status){
+ //   callback()
+//  }
+//}
+//function hello(msg,second) {
+//  console.log(msg, second)
+//}
+
+//my(true, hello.bind(this, "yea good!!", "2nd argugment"))
+
+
 function my(status, callback) {
   if(status){
     callback()
   }
 }
-function hello(msg,second) {
-  console.log(msg, second)
+function hello(...args) {
+  console.log(args)
 }
 
-my(true, hello.bind(this, "yea good!!", "2nd argugment"))
+my(true, hello.bind(this, "yea good!!", "2nd argugment", "third argument", false, [22,55,66,88]))
