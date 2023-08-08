b7fa813be54b6a283300b4af4abef11a599aeedf
aniket-bhadra <aniketbhadra98320@gmail.com>
2023-08-08 18:53:57 +0530

    revsited of the outcome what will happen if we put await infront of a regular function

--- trickyJs2.js
+++ trickyJs2.js
@@ -1,11 +1,58 @@
-function hello() {
-    console.log(name1); //undefined
-  }
-  hello()
-  var name1 = 10; 
+// function hello() {
+//     console.log(name1); //undefined
+//   }
+//   hello()
+//   var name1 = 10; 
 
-  function hello() {
-    console.log(age2); //age2 is not defined: error
-  }
-  hello()
-  let age2 = 10;
+//   function hello() {
+//     console.log(age2); //age2 is not defined: error
+//   }
+//   hello()
+//   let age2 = 10;
+
+//synchronous flow--
+
+
+// let numbers = 0;
+// function hello() {
+//   numbers = 25;
+//    console.log(`numbers is ${numbers}`)
+//   return numbers;
+// }
+
+// function testing(){
+//   const value = hello()
+//   console.log(`value is ${value}`)
+// }
+
+// testing()
+//console.log('finished');
+
+//------
+//ouput--numbers is 25
+//value is 25
+//finished
+
+//asynchronous flow----
+
+let numbers = 0;
+function hello() {
+  numbers = 25;
+  console.log(`numbers is ${numbers}`);
+  return numbers;
+}
+
+async function testing() {
+  const value = await hello();
+  console.log(`value is ${value}`);
+}
+
+testing();
+console.log('finished');
+
+//--------
+//output--
+// numbers is 25
+// finished
+// value is 25
+
